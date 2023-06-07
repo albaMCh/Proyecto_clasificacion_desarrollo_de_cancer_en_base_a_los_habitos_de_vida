34bc7ebc1e85b070d9e3396a8f07657177698de2
albamolera <albamolerachacon@gmail.com>
2023-06-07 13:45:23 +0200

    cambio modelo predictivo

--- backend/main.py
+++ backend/main.py
@@ -10,17 +10,16 @@ from pydantic import create_model
 app = FastAPI()
 
 # Load trained Pipeline
-model = load_model("lightgbm")
+model = load_model("knn")
 
 # Create input/output pydantic models
-input_model = create_model("lightgbm_input", **{'Patient Id': 'P650', 'Age': 44, 'Gender': 1, 'Air Pollution': 2, 'Alcohol use': 3, 'Dust Allergy': 2, 'OccuPational Hazards': 1, 'Genetic Risk': 3, 'chronic Lung Disease': 2, 'Balanced Diet': 1, 'Obesity': 2, 'Smoking': 7, 'Passive Smoker': 6, 'Chest Pain': 2, 'Coughing of Blood': 2, 'Fatigue': 2, 'Weight Loss': 2, 'Shortness of Breath': 3, 'Wheezing': 2, 'Swallowing Difficulty': 1, 'Clubbing of Finger Nails': 2, 'Frequent Cold': 3, 'Dry Cough': 2, 'Snoring': 3})
-output_model = create_model("lightgbm_output", prediction='Low')
+input_model = create_model("knn_input", **{'Patient Id': 'P789', 'Age': 39, 'Gender': 2, 'Air Pollution': 6, 'Alcohol use': 8, 'Dust Allergy': 7, 'OccuPational Hazards': 7, 'Genetic Risk': 7, 'chronic Lung Disease': 6, 'Balanced Diet': 7, 'Obesity': 7, 'Smoking': 8, 'Passive Smoker': 7, 'Chest Pain': 7, 'Coughing of Blood': 9, 'Fatigue': 3, 'Weight Loss': 2, 'Shortness of Breath': 4, 'Wheezing': 1, 'Swallowing Difficulty': 4, 'Clubbing of Finger Nails': 2, 'Frequent Cold': 4, 'Dry Cough': 2, 'Snoring': 3})
+output_model = create_model("knn_output", prediction='Low')
 
 
 # Define predict function
 @app.post("/predict", response_model=output_model)
 def predict(data: input_model):
-    print(data)
     data = pd.DataFrame([data.dict()])
     predictions = predict_model(model, data=data)
     return {"prediction": predictions["prediction_label"].iloc[0]}
