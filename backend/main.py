4c432b2f6a4df8a3fffd9e9d0bc85bc61da0a845
albamolera <albamolerachacon@gmail.com>
2023-06-15 11:41:48 +0200

    pruebas carga modelo

--- backend/main.py
+++ backend/main.py
@@ -6,16 +6,20 @@ from fastapi import FastAPI
 import uvicorn
 from pydantic import create_model
 import sys
-import path
+from pathlib import Path
 
 # Create the app
 app = FastAPI()
 
-dir = path.Path(__file__).abspath()
-sys.path.append(dir.parent.parent)
+#dir = path.Path(__file__).abspath()
+#sys.path.append(dir.parent.parent)
 
 # Load trained Pipeline
-model = load_model("knn")
+
+path = Path(__file__).parent / 'knn'
+print('==================================')
+print(path)
+model = load_model(path)
 
 # Create input/output pydantic models
 input_model = create_model("knn_input", **{'Patient Id': 'P789', 'Age': 39, 'Gender': 2, 'Air Pollution': 6, 'Alcohol use': 8, 'Dust Allergy': 7, 'OccuPational Hazards': 7, 'Genetic Risk': 7, 'chronic Lung Disease': 6, 'Balanced Diet': 7, 'Obesity': 7, 'Smoking': 8, 'Passive Smoker': 7, 'Chest Pain': 7, 'Coughing of Blood': 9, 'Fatigue': 3, 'Weight Loss': 2, 'Shortness of Breath': 4, 'Wheezing': 1, 'Swallowing Difficulty': 4, 'Clubbing of Finger Nails': 2, 'Frequent Cold': 4, 'Dry Cough': 2, 'Snoring': 3})
